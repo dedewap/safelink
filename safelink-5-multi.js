6120419abcb5f283db559ee72a21feb1522aa04c
dedewap <dedesaputr@ymail.com>
2017-03-11 13:06:22 +0700

    Update safelink-5-multi.js

--- safelink-5-multi.js
+++ safelink-5-multi.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="testing.html"}
-if(x=="2"){xxx="testing2.html"}
-if(x=="3"){xxx="testing3.html"}
-if(x=="4"){xxx="testing4.html"}
-if(x=="5"){xxx="testing5.html"}a_to_ve[i].href="http://layarkaco21.blogspot.com/2017/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="avast.html"}
+if(x=="2"){xxx="avira.html"}
+if(x=="3"){xxx="eset.html"}
+if(x=="4"){xxx="avg.html"}
+if(x=="5"){xxx="smadav.html"}a_to_ve[i].href="https://layarkaco21.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
